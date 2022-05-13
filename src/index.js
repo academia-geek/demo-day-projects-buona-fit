a9fd4afae8cc4df711bb4d4edc03bf8d1f9b7120
CAndres438 <candresortiz123@gmail.com>
2022-05-13 00:20:34 -0500

    Configuración global para la Aplicación

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
+import { Provider } from 'react-redux';
+import { store } from './redux/store/store';
+import AppRoutes from './routes/AppRoutes';
+import GlobalStyle from './styled/GlobalStyle';
 
 
 
 
 ReactDOM.render(
-    <App/>,
+  <Provider store={store}>
+    <GlobalStyle/>
+    <AppRoutes/>
+  </Provider>,
   document.getElementById('root')
 );
 
